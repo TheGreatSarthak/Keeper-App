d8e84870602c2d9240ab1b3685eb0ee8ac9b70ba
Sarthak Sharma <sarthak2232@gmail.com>
2023-12-14 22:06:27 +0530

    using map function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map((value) => (
+        <Note key={value.key} title={value.title} content={value.content} />
+      ))}
       <Footer />
     </div>
   );
